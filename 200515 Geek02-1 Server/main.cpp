57dee49c646c5843bf8db1ccce161adbfe7557fc
jyupiter <aotoharuto0750@yahoo.co.jp>
2020-05-15 16:29:42 +0900

    疑似HTTPサーバに改造

--- 200515 Geek02-1 Server/main.cpp
+++ 200515 Geek02-1 Server/main.cpp
@@ -1,5 +1,6 @@
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include<WinSock2.h>
 #include<stdio.h>
@@ -19,6 +20,9 @@ int main(void)
 	int len;
 	
 	BOOL yes = 1;//TIME_WAIT回避用
+
+	char buf[2048];
+	char inbuf[2048];
 
 	//winsock2の初期化
 	int err = WSAStartup(MAKEWORD(2, 0), &wsaData);
@@ -87,6 +91,16 @@ int main(void)
 		return 1;
 	}
 	
+	//応答用のHTTPメッセージを作成
+	memset(buf, 0, sizeof(buf));
+	_snprintf(buf, sizeof(buf),
+		"HTTP/1.0 200 OK\r\n"
+		"Content-Length: 20\r\n"
+		"Content-Type: text/html\r\n"
+		"\r\n"
+		"HELLO\r\n");
+
+	
 	while (1)//ここがあると単純なTCPクライアントからの接続要求を複数回受け付けれる（何回も作っているから）
 	{
 		//TCPクライアントからの接続要求を受け付ける
@@ -100,12 +114,15 @@ int main(void)
 			return 1;
 		}
 
-		//どのクライアントからリクエストを受け取ったか
+		//どのクライアントからリクエストを受け取ったか表示
 		printf("accept connection from %s,port = %d\n", 
 			inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
+		memset(inbuf, 0, sizeof(inbuf));
+		recv(sock, inbuf, sizeof(inbuf), 0);
+
 		//5文字送信
-		int n = send(sock, "HELLO", 5, 0);//送りたいメッセージ、メッセージの長さ、
+		int n = send(sock, buf, strlen(buf), 0);//送りたいメッセージ、メッセージの長さ、
 		if (n < 1)
 		{
 			printf("send : %d\n", WSAGetLastError());
